a7a0eff2fd1821d31348d4ffa02d31acc0274c50
NathanPradelle <nathan.pradelle77@gmail.com>
2025-08-20 16:22:06 +0200

    retesting shared component

--- src/components/Test.tsx
+++ src/components/Test.tsx
@@ -1,5 +1,5 @@
 import React from "react";
 
 export const Test: React.FC = () => {
-  return <div style={{ padding: "1rem"}}>Test depuis shared</div>;
+  return <div style={{ padding: "1rem"}}> Test du shared ! </div>;
 };
